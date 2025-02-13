636d79db6f115966bd3ac1e10bde9895e49636a7
M Agung Dirgantara Azly Bagus Wibawa <agungmasda29@gmail.com>
2025-02-13 19:41:21 +0700

    feat(smtp): use environment variables for SMTP configuration

--- src/config/smtp.js
+++ src/config/smtp.js
@@ -18,11 +18,11 @@
  * @type {SMTPConfig}
  */
 module.exports = {
-  port: 587,
-  host: '',
-  from: '',
+  port: process.env.SMTP_PORT,
+  host: process.env.SMTP_HOST,
+  from: process.env.SMTP_FROM,
   auth: {
-    user: '',
-    pass: '',
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 };
